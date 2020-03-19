4b472d9bdde2495f9da4bb902ae39488de226564
unknown <yitucloud@gmail.com>
2020-03-19 19:56:20 -0400

    two

--- Azure_Devops/main.tf
+++ Azure_Devops/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
    region  = "us-east-2"
    access_key = "AKIA3SKVBDL54TUOXOWT"
    secret_key = "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"
+   skip_requesting_account_id = true
 }
 
 resource "aws_vpc" "main" {
